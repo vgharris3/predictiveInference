f98f535db51d26e394e18c091196bf7c91248152
Gabe Harris <gabe@tucsonmath.com>
2021-10-06 08:17:09 -0700

    cleaning up rpredNormReg().  cleaner output list, working on comparison examples

--- R/rpredNormReg.R
+++ R/rpredNormReg.R
@@ -63,25 +63,25 @@ rpredNormReg = function(S=1,Xpred,X,y,beta0,Sigma0,nu0=1,s20=1,gprior = TRUE){
     Hg = (g/(g+1))*X%*%solve(t(X)%*%X)%*%t(X)
     SSRg = t(y)%*%( diag(1,nrow=n) - Hg)%*%y
 
-    s2 = 1/rgamma(S, (nu0 + n)/2, (nu0*s20 + SSRg)/2 )
-    result$sigma2 = s2
+    sigma2 = 1/rgamma(S, (nu0 + n)/2, (nu0*s20 + SSRg)/2 )
+    #result$sigma2 = s2
 
     Vb = g*solve(t(X)%*%X)/(g+1)
     Eb = Vb%*%t(X)%*%y    #Posterior means of the betas (matches Hoff p. 159 lower half)
                           #using apply(beta,2,sd) shows sd of these parameters close to Hoff
 
-    E = matrix(rnorm(S*p,0,sqrt(s2)),S,p)
-    beta = t( t(E%*%chol(Vb)) + c(Eb))
+    E = matrix(rnorm(S*p,0,sqrt(sigma2)),S,p)
+    betas = t( t(E%*%chol(Vb)) + c(Eb))
 
-    result$betas = beta
+    #result$betas = betas
 
-    predictions = Xpred%*%t(beta)
+    predictions = Xpred%*%t(betas)
 
     for(i in 1:nrow(Xpred)){
-      predictions[i,] = predictions[i,] + rnorm(S,0,sqrt(s2))
+      predictions[i,] = predictions[i,] + rnorm(S,0,sqrt(sigma2))
     }
 
-    result$predictions = predictions
+    #result$predictions = predictions
 
 
 
@@ -105,33 +105,35 @@ rpredNormReg = function(S=1,Xpred,X,y,beta0,Sigma0,nu0=1,s20=1,gprior = TRUE){
 
     ## starting value
     #set.seed(1)
-    sigma2<- s20                #starting value for sigma2
+    sig2<- s20                #starting value for sigma2
     beta.0 = beta0              #starting value for beta, which gets reused in computing E(beta|y,X,sig2)
 
     ## MCMC algorithm
     for( scan in 1:S) {
 
       #update beta                                        #Formulas and steps Hoff p. 155
-      V.beta<- solve(  iSigma.0 + XtX/sigma2 )            #Conditional variance of the regression coefficients
-      E.beta<- V.beta%*%( iSigma.0%*%beta.0 + t(X)%*%y/sigma2 ) #Conditional mean of the variance coefficients
+      V.beta<- solve(  iSigma.0 + XtX/sig2 )            #Conditional variance of the regression coefficients
+      E.beta<- V.beta%*%( iSigma.0%*%beta.0 + t(X)%*%y/sig2 ) #Conditional mean of the variance coefficients
       beta<-t(rmvnorm(1, E.beta,V.beta) )                 #Gibbs sampler step:  update beta
 
       #update sigma2
       nu.n<- nu.0+n                                       #numerator of 1st term of inverse gamma sample
       ss.n<-nu.0*sigma2.0 + sum(  (y-X%*%beta)^2 )        #numerator of 2nd term of inverse gamma sample
-      sigma2<-1/rgamma(1,nu.n/2, ss.n/2)                  #Gibbs ampler step:  update sigma^2
+      sig2<-1/rgamma(1,nu.n/2, ss.n/2)                  #Gibbs sampler step:  update sigma^2
 
       #save results of this scan
       beta.post[scan,]<-beta                              #Store updated beta in current row of beta matrix
-      sigma2.post[scan]<-sigma2                           #Store updated sigma^2 in current position in sigma^2 vector
+      sigma2.post[scan]<-sig2                           #Store updated sigma^2 in current position in sigma^2 vector
     }
 
     round( apply(beta.post,2,mean), 3)                  #compute mean of Gibbs sampled betas (for check)
 
 #    result = list(beta.post, Xpred%*%t(beta.post))
 
-    result$betas = beta.post
-    result$sigma2 = sigma2.post
+    #result$betas = beta.post
+    betas = beta.post
+    #result$sigma2 = sigma2.post
+    sigma2 = sigma2.post
 
     predictions = Xpred%*%t(beta.post)
 
@@ -143,5 +145,7 @@ rpredNormReg = function(S=1,Xpred,X,y,beta0,Sigma0,nu0=1,s20=1,gprior = TRUE){
 
   }
 
-  return(result)
+
+  result_list = list("betas" = betas, "sigma2" = sigma2, "predictions" = predictions)
+  return(result_list)
 }
